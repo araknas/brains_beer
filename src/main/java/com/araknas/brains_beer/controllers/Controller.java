1c4fe886d2d1bd80797baf544f82ff8b2862faa2
Giedrius Valančius <gievln@telesoftas.com>
2017-10-06 16:16:55 +0300

    Initial commit

---
+++ src/main/java/com/araknas/brains_beer/controllers/Controller.java
@@ -0,0 +1,10 @@
+package com.araknas.brains_beer.controllers;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Giedrius on 2017-10-06.
+ */
+@Component
+public class Controller {
+}
